408ba288052a581625f60d0e4bf8959cbe6e505a
Miksiu <miksiu120@gmail.com>
2025-01-23 19:49:02 +0100

    header(fix): correct header

---
+++ src/app/pages/welcome/instruction/instruction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-instruction',
+  templateUrl: './instruction.component.html',
+  styleUrls: ['./instruction.component.scss'],
+})
+export class InstructionComponent {
+  // Component logic goes here
+}
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { InstructionComponent } from './instruction/instruction.component';
 
 @Component({
   selector: 'page-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
+  imports: [HeaderComponent, InstructionComponent],
 })
 export class WelcomeComponent {
   // Component logic goes here
