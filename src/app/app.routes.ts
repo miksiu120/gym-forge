2ecb178b18c99878448721ac622956f638d76082
miksiuLaptop <miksiu120@gmail.com>
2025-01-24 10:55:08 +0100

    feat(footer): add footer component and update app layout; implement 404 Not Found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { CalculatorsComponent } from './pages/calculators/calculators.component'
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'calculators', component: CalculatorsComponent },
+  { path: '**', component: NotFoundComponent },
 ];
