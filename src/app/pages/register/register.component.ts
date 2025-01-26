cebe9e4aae93bcc4db6420537055d767fbb7347a
Miksiu <miksiu120@gmail.com>
2025-01-26 18:33:39 +0100

    feat(register): implement registration form and layout; enhance calculator recommendation component styling

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterModule, RouterLinkActive } from '@angular/router';
 @Component({
     selector: 'page-register',
     templateUrl: './register.component.html',
-    styleUrls: ['./register.component.scss']
+    styleUrls: ['./register.component.scss'],
+    imports: [RouterModule, RouterLinkActive]
 })
 export class RegisterComponent {
     constructor() { }
