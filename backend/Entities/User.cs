89fda31f5e0d5fe9dfc8eb2313f3ce46eea07a00
Miksiu <miksiu120@gmail.com>
2025-02-20 20:01:54 +0100

    add backend

---
+++ backend/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace WorkPlanner.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nickanme { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+
+        public string Role { get; set; }
+
+    }
+}
---
+++ backend/Models/CreateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkPlanner.Models
+{
+    public class CreateUserDto
+    {
+        public string Nickanme { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string? Weight { get; set; }
+        public string? Height { get; set; }
+    }
+}
